4538d4cac735e5ea96eebac52c0df68c27bf0326
Mocha-Lee <mocha_lee@qq.com>
2021-01-23 15:24:27 +0800

    fix return str

--- app/src/main/java/leon/qujing/api/Invoke.java
+++ app/src/main/java/leon/qujing/api/Invoke.java
@@ -1,11 +1,16 @@
 package leon.qujing.api;
 
+import com.alibaba.fastjson.JSON;
+
 import java.lang.reflect.Method;
 import java.util.Map;
 
 import leon.qujing.QuJingServer;
 import leon.qujing.XposedEntry;
 import leon.qujing.handler.ObjectHandler;
+import leon.qujing.utils.Utils;
+
+import static leon.qujing.QuJingServer.parsers;
 
 //处理反射调用
 public class Invoke implements QuJingServer.Operation {
@@ -24,12 +29,21 @@ public class Invoke implements QuJingServer.Operation {
                 params[i] = QuJingServer.parsers.get(parms.get("parser" + i)).parse(parms.get("param" + i));
             }
             m.setAccessible(true);
-            sb.append(ObjectHandler.saveObject(m.invoke(thisobj, params)));
+            sb.append(translate(m.invoke(thisobj, params)));
         } catch (Exception e) {
             sb.append("执行异常，报错信息: " + e.getLocalizedMessage());
         }
         return sb.toString();
     }
+
+    private String translate(Object obj) {
+        //Write your translator here.
+        if (obj == null) return "null";
+        if (obj.getClass().getName().equals("java.lang.String")) return obj.toString();
+        else if(Utils.getTypeSignature(obj.getClass()).equals("[B"))return parsers.get("[B").generate(obj);
+        else return JSON.toJSONString(obj);
+    }
+
     public static boolean isMe(){
         StackTraceElement[] stacks = Thread.currentThread().getStackTrace();
         for (int i = 4; i <stacks.length ; i++) {
